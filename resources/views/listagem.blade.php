a68c67272af61440e911a0053f48d8ce1ec3f96f
Anderson Lairton <lairton.anderson@gmail.com>
2020-05-05 20:33:39 -0300

    fazendo foreach no blade

--- resources/views/listagem.blade.php
+++ resources/views/listagem.blade.php
@@ -3,7 +3,7 @@
 <h1>Listagem de produtos</h1>
 <!-- view alimentada pelo controller produtos -->
 <table class="table">
-    <?php foreach ($produtos as $p) : ?>
+    @foreach ($produtos as $p) 
         <tr>
             <td> {{$p->nome}} </td>
             <td>R$ {{ $p->valor}}</td>
@@ -13,7 +13,7 @@
                 <a href="produtos/detalhes/{{$p->id}}"><span class="glyphicon glyphicon-search"></span>ver</a>
             </td>
         </tr>
-    <?php endforeach ?>
+    @endforeach
 </table>
 </div>
 @stop
