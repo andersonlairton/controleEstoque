7a32c4d21e3f41a74a1f4d27fadec5151c05bc8a
Anderson Lairton <lairton.anderson@gmail.com>
2020-05-05 14:54:15 -0300

    criando o seeder da tabela produtos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         $this->call(ProdutoTableSeeder::class);
+    }
+}
+
+class ProdutoTableSeeder extends Seeder{//função que automatiza a inserção de dados
+    public function run(){
+
+        //fazendo o insert
+        DB::insert('insert into produtos
+        (nome,quantidade,valor,descricao)
+        values(?,?,?,?)',
+        ['Geladeira',2, 0,'Side by Side com gelo e dispacer na porta']);
+
+        DB::insert('insert into produtos
+        (nome,quantidade,valor,descricao)
+        values(?,?,?,?)',
+        ['Fogão',5,950.99,'painel automatico e forno eletrico']);
+
+        DB::insert('insert into produtos
+        (nome,quantidade,valor,descricao)
+        values(?,?,?,?)',
+        ['Microondas',1,1520.99,'Avisa no sms quando a pipoca esta pronta']);
     }
 }
