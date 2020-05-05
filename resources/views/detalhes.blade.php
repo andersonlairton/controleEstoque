c4fd5bd6502f8702622d3148f013c14df15cae7c
Anderson Lairton <lairton.anderson@gmail.com>
2020-05-05 20:01:48 -0300

    definindo layout no blade

---
+++ resources/views/detalhes.blade.php
@@ -0,0 +1,15 @@
+@extends('principal')
+@section('conteudo')
+<h1>Detalhes do Produto <?= $p[0]->nome ?></h1>
+<ul>
+    <li>
+        <b>Valor:</b>R$ <?= $p[0]->valor ?>
+    </li>
+    <li>
+        <b>Descrição:</b><?= $p[0]->descricao ?>
+    </li>
+    <li>
+        <b>Quantidade em estoque: </b><?= $p[0]->quantidade ?>
+    </li>
+</ul>
+@stop
